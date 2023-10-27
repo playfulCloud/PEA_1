0a32643c5b2e202d90025d105d3cbed2c90560db
JAkub <generovic@gmail.com>
2023-10-27 14:31:00 +0200

    task + menu

--- PEA_1/Utils/Presentation/Menu.cpp
+++ PEA_1/Utils/Presentation/Menu.cpp
@@ -2,4 +2,23 @@
 // Created by Jakub on 27.10.2023.
 //
 
+#include <iostream>
 #include "Menu.h"
+
+/*TODO
+ * 1. Wczytywanie danych z pliku.
+ * 2. Wygenerowanie losowych danych.
+ * 3. Wyświetlenie ostatnio wczytanych daynch.
+ * 4. Wyświetlenie ostatnio wygenerowanych danych.
+ * 5. Uruchomienie algorytmu dla wygenerowanych danych bądź wczytanych danych.
+ * Wyświetlenie Wynik: Długość ścieżki: X  Ciąg Wierzchołków: X-Y-Z Czas Wykonywania: X.XXms
+ * */
+
+
+
+void Menu::displayMenu() {
+    int gate = 0;
+//    while(gate != 7){
+//        std::cout << ""
+//    }
+}
--- PEA_1/Utils/Presentation/Menu.h
+++ PEA_1/Utils/Presentation/Menu.h
@@ -6,7 +6,15 @@
 #define PEA_1_MENU_H
 
 
+#include "../../Algorithms/BruteForce/BruteForce_Implementation.h"
+#include "../Reader/FileReader.h"
+
 class Menu {
+public:
+    void displayMenu();
+    BruteForce_Implementation *bruteForceImplementation;
+    FileReader *fileReader;
+
 
 };
 
